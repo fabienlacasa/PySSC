ec024972bcbda805ce2b9d47033a9c21f020d546
fabien-UX310UQK <fabien.lacasa@unige.ch>
2019-05-29 19:35:43 +0200

    Progress on new Sij function

--- pyssc.py
+++ pyssc.py
@@ -118,22 +118,24 @@ def turboSij(zstakes=default_zstakes, cosmo_params=default_cosmo_params):
 # Inputs : window functions (format: see below), cosmological parameters (dictionnary as in CLASS's wrapper classy)
 # Format for window functions : one table of redshifts with size nz, one 2D table for the first window function with shape (nbin1,nz), one 2D table for the second window function with shape (nbin2,nz)
 # Output : Sij matrix (size: nbin1 x nbin2)
-def Sij(z_arr, window1_arr, window2_arr, cosmo_params=default_cosmo_params):
+def Sij(z_arr, window1, window2, cosmo_params=default_cosmo_params):
 
     # Assert everything as the good type and shape, and find number of redshifts, bins etc
     zz   = np.asarray(z_arr)
-    win1 = np.asarray(window1_arr)
-    win2 = np.asarray(window2_arr)
+    win1 = np.asarray(window1)
+    win2 = np.asarray(window2)
     
     assert zz.ndim==1, 'z_arr must be a 1-dimensional array'
-    assert win1.ndim==2, 'window1_arr must be a 2-dimensional array'
-    assert win2.ndim==2, 'window2_arr must be a 2-dimensional array'
+    assert win1.ndim==2, 'window1 must be a 2-dimensional array'
+    assert win2.ndim==2, 'window2 must be a 2-dimensional array'
     
-    nz = len(zz)
+    nz    = len(zz)
     nbin1 = win1.shape[0]
     nbin2 = win2.shape[0]
-    assert win1.shape[1]==nz, 'window1_arr must have shape (nbins,nz)'
-    assert win2.shape[1]==nz, 'window1_arr must have shape (nbins,nz)'
+    assert win1.shape[1]==nz, 'window1 must have shape (nbins,nz)'
+    assert win2.shape[1]==nz, 'window2 must have shape (nbins,nz)'
+    
+    assert zz.min()>0, 'z_arr must have values > 0'
     
     # Run CLASS
     cosmo = Class()
@@ -143,8 +145,50 @@ def Sij(z_arr, window1_arr, window2_arr, cosmo_params=default_cosmo_params):
     cosmo.compute() 
     h = cosmo.h() #for  conversions Mpc/h <-> Mpc
     
+    # Define arrays of r(z), k, P(k)...
+    comov_dist = cosmo.z_of_r(zz)[0] #In Mpc
+    keq        = 0.02/h                        #Equality matter radiation in 1/Mpc (more or less)
+    klogwidth  = 10                            #Factor of width of the integration range. 10 seems ok ; going higher needs to increase nk_fft to reach convergence (fine cancellation issue noted in Lacasa & Grain)
+    kmin       = min(keq,1./comov_dist.max())/klogwidth
+    kmax       = max(keq,1./comov_dist.min())*klogwidth
+    nk_fft     = 2**11                         #seems to be enough. Increase to test precision, reduce to speed up.
+    k_4fft     = np.linspace(kmin,kmax,nk_fft) #linear grid on k, as we need to use an FFT
+    Deltak     = kmax - kmin
+    Dk         = Deltak/nk_fft
+    Pk_4fft    = np.zeros(nk_fft)
+    for ik in range(nk_fft):
+        Pk_4fft[ik] = cosmo.pk(k_4fft[ik],0.)  #In Mpc^3
+    dr_fft     = np.linspace(0,nk_fft//2,nk_fft//2+1)*2*pi/Deltak
+    
+    # Compute necessary FFTs and make interpolation functions
+    fft0       = np.fft.rfft(Pk_4fft)*Dk
+    dct0       = fft0.real ; dst0 = -fft0.imag
+    Pk_dct     = interp1d(dr_fft,dct0,kind='cubic')
+    
+    # Compute sigma^2(z1,z2)
+    sigma2_nog = np.zeros((nz,nz))
+    #First with P(k,z=0) and z1<=z2
+    for iz in range(nz):
+        r1 = comov_dist[iz]
+        for jz in range(iz,nz):
+            r2                = comov_dist[jz]
+            rsum              = r1+r2
+            rdiff             = abs(r1-r2)
+            Icp0              = Pk_dct(rsum) ; Icm0 = Pk_dct(rdiff)
+            sigma2_nog[iz,jz] = (Icm0-Icp0)/(2*pi**2*r1*r2)
+    #Now fill by symmetry and put back growth functions
+    sigma2     = np.zeros((nz,nz))
+    for iz in range(nz):
+        growth1 = cosmo.scale_independent_growth_factor(zz[iz])
+        for jz in range(nz):
+            growth2       = cosmo.scale_independent_growth_factor(zz[jz])
+            sigma2[iz,jz] = sigma2_nog[min(iz,jz),max(iz,jz)]*growth1*growth2            
+    
     # Compute Sij finally
-    Sij = np.zeros((nbin1,nbin2))
+    Sij        = np.zeros((nbin1,nbin2))
+    for i1 in range(nbin1):
+        for i2 in range(nbin2):
+            Sij[i1,i2] = 0
     
     return Sij
 
