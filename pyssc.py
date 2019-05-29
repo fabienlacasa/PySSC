55e00ca55c36044b2c7024b91e7594c9da0bd00e
fabien-UX310UQK <fabien.lacasa@unige.ch>
2019-05-29 18:12:11 +0200

    Started a new Sij function accounting for window functions

--- pyssc.py
+++ pyssc.py
@@ -14,7 +14,10 @@ from classy import Class
 default_zstakes = [0.9,1]
 default_cosmo_params = {'omega_b':0.022,'omega_cdm':0.12,'H0':67.,'n_s':0.96,'sigma8':0.81}
 
-# Routine to compute the Sij matrix
+# Routine to compute the Sij matrix with top-hat disjoint redshift window functions
+# example : galaxy clustering with perfect/spectroscopic redshift determinations so that bins are sharp.
+# Inputs : stakes of the redshift bins (array), cosmological parameters (dictionnary as in CLASS's wrapper classy)
+# Output : Sij matrix (size: nbins x nbins)
 def turboSij(zstakes=default_zstakes, cosmo_params=default_cosmo_params):
 
     # Run CLASS
@@ -108,6 +111,41 @@ def turboSij(zstakes=default_zstakes, cosmo_params=default_cosmo_params):
             Fsum       = Fp1p2 - Fp1m2 - Fm1p2 + Fm1m2
             Sij[j1,j2] = pref1 * pref2 * Fsum/(4*pi**2)
 
+    return Sij
+
+# Routine to compute the Sij matrix with general window functions given as tables
+# example : weak lensing, or galaxy clustering with redshift errors
+# Inputs : window functions (format: see below), cosmological parameters (dictionnary as in CLASS's wrapper classy)
+# Format for window functions : one table of redshifts with size nz, one 2D table for the first window function with shape (nbin1,nz), one 2D table for the second window function with shape (nbin2,nz)
+# Output : Sij matrix (size: nbin1 x nbin2)
+def Sij(z_arr, window1_arr, window2_arr, cosmo_params=default_cosmo_params):
+
+    # Assert everything as the good type and shape, and find number of redshifts, bins etc
+    zz   = np.asarray(z_arr)
+    win1 = np.asarray(window1_arr)
+    win2 = np.asarray(window2_arr)
+    
+    assert zz.ndim==1, 'z_arr must be a 1-dimensional array'
+    assert win1.ndim==2, 'window1_arr must be a 2-dimensional array'
+    assert win2.ndim==2, 'window2_arr must be a 2-dimensional array'
+    
+    nz = len(zz)
+    nbin1 = win1.shape[0]
+    nbin2 = win2.shape[0]
+    assert win1.shape[1]==nz, 'window1_arr must have shape (nbins,nz)'
+    assert win2.shape[1]==nz, 'window1_arr must have shape (nbins,nz)'
+    
+    # Run CLASS
+    cosmo = Class()
+    dico_for_CLASS = cosmo_params
+    dico_for_CLASS['output'] = 'mPk'
+    cosmo.set(dico_for_CLASS)
+    cosmo.compute() 
+    h = cosmo.h() #for  conversions Mpc/h <-> Mpc
+    
+    # Compute Sij finally
+    Sij = np.zeros((nbin1,nbin2))
+    
     return Sij
 
 # End of pyssc.py
