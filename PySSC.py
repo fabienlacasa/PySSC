e811d2b9ac44b58a246635eb00426ab5d1c1e0c0
fabien-UX310UQK <fabien.lacasa@unige.ch>
2019-09-03 19:36:12 +0200

    Added the possibility to feed a precomputed cosmology (distances, P(k) etc) into PySSC's routines, instead of it computing stuff internally with CLASS. Added explanations and an example in examples.ipynb

--- PySSC.py
+++ PySSC.py
@@ -18,14 +18,18 @@ default_cosmo_params = {'omega_b':0.022,'omega_cdm':0.12,'H0':67.,'n_s':0.96,'si
 # example : galaxy clustering with perfect/spectroscopic redshift determinations so that bins are sharp.
 # Inputs : stakes of the redshift bins (array), cosmological parameters (dictionnary as in CLASS's wrapper classy)
 # Output : Sij matrix (size: nbins x nbins)
-def turboSij(zstakes=default_zstakes, cosmo_params=default_cosmo_params):
+def turboSij(zstakes=default_zstakes, cosmo_params=default_cosmo_params,cosmo_Class=None):
 
-    # Run CLASS
-    cosmo = Class()
-    dico_for_CLASS = cosmo_params
-    dico_for_CLASS['output'] = 'mPk'
-    cosmo.set(dico_for_CLASS)
-    cosmo.compute() 
+    # If the cosmology is not provided (in the same form as CLASS), run CLASS
+    if cosmo_Class is None:
+        cosmo = Class()
+        dico_for_CLASS = cosmo_params
+        dico_for_CLASS['output'] = 'mPk'
+        cosmo.set(dico_for_CLASS)
+        cosmo.compute()
+    else:
+        cosmo = cosmo_Class
+
     h = cosmo.h() #for  conversions Mpc/h <-> Mpc
 
     # Define arrays of z, r(z), k, P(k)...
@@ -118,11 +122,11 @@ def turboSij(zstakes=default_zstakes, cosmo_params=default_cosmo_params):
 # Inputs : window functions (format: see below), cosmological parameters (dictionnary as in CLASS's wrapper classy)
 # Format for window functions : one table of redshifts with size nz, one 2D table for the collection of window functions with shape (nbins,nz)
 # Output : Sij matrix (shape: nbins x nbins)
-# Equation used :  Sij = 1/(2*pi^2) int k^2 dk P(k) U(i,k)/Inorm(i) U(j,k)/Inorm(j)
-# with Inorm(i) = int dV window(i,z)^2 and U(i,k) = int dV window(i,z)^2 growth(z) j_0(kr)
-# This can also be seen as an angular power spectrum : Sij = C(ell=0,i,j)/4pi
-# with C(ell=0,i,j) = 2/pi int k^2 dk P(k) U(i,k)/Inorm(i) U(j,k)/Inorm(j)
-def Sij(z_arr, windows, cosmo_params=default_cosmo_params,precision=10):
+## Equation used :  Sij = 1/(2*pi^2) int k^2 dk P(k) U(i,k)/Inorm(i) U(j,k)/Inorm(j)
+## with Inorm(i) = int dV window(i,z)^2 and U(i,k) = int dV window(i,z)^2 growth(z) j_0(kr)
+## This can also be seen as an angular power spectrum : Sij = C(ell=0,i,j)/4pi
+## with C(ell=0,i,j) = 2/pi int k^2 dk P(k) U(i,k)/Inorm(i) U(j,k)/Inorm(j)
+def Sij(z_arr, windows, cosmo_params=default_cosmo_params,precision=10,cosmo_Class=None):
 
     # Assert everything as the good type and shape, and find number of redshifts, bins etc
     zz  = np.asarray(z_arr)
@@ -137,12 +141,16 @@ def Sij(z_arr, windows, cosmo_params=default_cosmo_params,precision=10):
     
     assert zz.min()>0, 'z_arr must have values > 0'
     
-    # Run CLASS
-    cosmo = Class()
-    dico_for_CLASS = cosmo_params
-    dico_for_CLASS['output'] = 'mPk'
-    cosmo.set(dico_for_CLASS)
-    cosmo.compute() 
+    # If the cosmology is not provided (in the same form as CLASS), run CLASS
+    if cosmo_Class is None:
+        cosmo = Class()
+        dico_for_CLASS = cosmo_params
+        dico_for_CLASS['output'] = 'mPk'
+        cosmo.set(dico_for_CLASS)
+        cosmo.compute()
+    else:
+        cosmo = cosmo_Class
+
     h = cosmo.h() #for  conversions Mpc/h <-> Mpc
     
     # Define arrays of r(z), k, P(k)...
@@ -201,11 +209,11 @@ def Sij(z_arr, windows, cosmo_params=default_cosmo_params,precision=10):
 # Alternative routine to compute the Sij matrix with general window functions given as tables
 # Inputs : window functions, cosmological parameters, same format as Sij()
 # Output : Sij matrix (shape: nbins x nbins)
-# Equation used : Sij = int dV1 dV2 window(i,z1)^2/Inorm(i) window(j,z2)^2/Inorm(j) sigma2(z1,z2)
-# with Inorm(i) = int dV window(i,z)^2 and sigma2(z1,z2) = 1/(2*pi^2) int k^2 dk P(k|z1,z2) j_0(kr1) j_0(kr2)
-# which can be rewritten as sigma2(z1,z2) = 1/(2*pi^2*r1r2) G(z1) G(z2) int dk P(k,z=0) [cos(k(r1-r2))-cos(k(r1+r2))]/2
-# which can be computed with an FFT
-def Sij_alt(z_arr, windows, cosmo_params=default_cosmo_params):
+## Equation used : Sij = int dV1 dV2 window(i,z1)^2/Inorm(i) window(j,z2)^2/Inorm(j) sigma2(z1,z2)
+## with Inorm(i) = int dV window(i,z)^2 and sigma2(z1,z2) = 1/(2*pi^2) int k^2 dk P(k|z1,z2) j_0(kr1) j_0(kr2)
+## which can be rewritten as sigma2(z1,z2) = 1/(2*pi^2*r1r2) G(z1) G(z2) int dk P(k,z=0) [cos(k(r1-r2))-cos(k(r1+r2))]/2
+## which can be computed with an FFT
+def Sij_alt(z_arr, windows, cosmo_params=default_cosmo_params,cosmo_Class=None):
 
     # Assert everything as the good type and shape, and find number of redshifts, bins etc
     zz  = np.asarray(z_arr)
@@ -220,12 +228,16 @@ def Sij_alt(z_arr, windows, cosmo_params=default_cosmo_params):
     
     assert zz.min()>0, 'z_arr must have values > 0'
     
-    # Run CLASS
-    cosmo = Class()
-    dico_for_CLASS = cosmo_params
-    dico_for_CLASS['output'] = 'mPk'
-    cosmo.set(dico_for_CLASS)
-    cosmo.compute() 
+    # If the cosmology is not provided (in the same form as CLASS), run CLASS
+    if cosmo_Class is None:
+        cosmo = Class()
+        dico_for_CLASS = cosmo_params
+        dico_for_CLASS['output'] = 'mPk'
+        cosmo.set(dico_for_CLASS)
+        cosmo.compute()
+    else:
+        cosmo = cosmo_Class
+
     h = cosmo.h() #for  conversions Mpc/h <-> Mpc
     
     # Define arrays of r(z), k, P(k)...
@@ -300,11 +312,11 @@ def Sij_alt(z_arr, windows, cosmo_params=default_cosmo_params):
 # Routine to compute the Sijkl matrix, i.e. the most general case with cross-spectra
 # Inputs : window functions, cosmological parameters, same format as Sij()
 # Output : Sijkl matrix (shape: nbins x nbins x nbins x nbins)
-# Equation used :  Sijkl = 1/(2*pi^2) \int kk^2 dkk P(kk) U(i,j;kk)/Inorm(i,j) U(k,l;kk)/Inorm(k,l)
-# with Inorm(i,j) = int dV window(i,z) window(j,z) and U(i,j;kk) = int dV window(i,z) window(j,z) growth(z) j_0(kk*r)
-# This can also be seen as an angular power spectrum : Sijkl = C(ell=0,i,j,k,l)/4pi
-# with C(ell=0,i,j,k,l) = 2/pi \int kk^2 dkk P(kk) U(i,j;kk)/Inorm(i,j) U(k,l;kk)/Inorm(k,l)
-def Sijkl(z_arr, windows, cosmo_params=default_cosmo_params,precision=10,tol=1e-3):
+## Equation used :  Sijkl = 1/(2*pi^2) \int kk^2 dkk P(kk) U(i,j;kk)/Inorm(i,j) U(k,l;kk)/Inorm(k,l)
+## with Inorm(i,j) = int dV window(i,z) window(j,z) and U(i,j;kk) = int dV window(i,z) window(j,z) growth(z) j_0(kk*r)
+## This can also be seen as an angular power spectrum : Sijkl = C(ell=0,i,j,k,l)/4pi
+## with C(ell=0,i,j,k,l) = 2/pi \int kk^2 dkk P(kk) U(i,j;kk)/Inorm(i,j) U(k,l;kk)/Inorm(k,l)
+def Sijkl(z_arr, windows, cosmo_params=default_cosmo_params,precision=10,tol=1e-3,cosmo_Class=None):
 
     # Assert everything as the good type and shape, and find number of redshifts, bins etc
     zz  = np.asarray(z_arr)
@@ -319,12 +331,16 @@ def Sijkl(z_arr, windows, cosmo_params=default_cosmo_params,precision=10,tol=1e-
     
     assert zz.min()>0, 'z_arr must have values > 0'
     
-    # Run CLASS
-    cosmo = Class()
-    dico_for_CLASS = cosmo_params
-    dico_for_CLASS['output'] = 'mPk'
-    cosmo.set(dico_for_CLASS)
-    cosmo.compute() 
+    # If the cosmology is not provided (in the same form as CLASS), run CLASS
+    if cosmo_Class is None:
+        cosmo = Class()
+        dico_for_CLASS = cosmo_params
+        dico_for_CLASS['output'] = 'mPk'
+        cosmo.set(dico_for_CLASS)
+        cosmo.compute()
+    else:
+        cosmo = cosmo_Class
+
     h = cosmo.h() #for  conversions Mpc/h <-> Mpc
     
     # Define arrays of r(z), k, P(k)...
