c1cfef33d00530d311b8f4ce9eb2fea190ef371f
BARATTA Philippe <baratta@cppm.in2p3.fr>
2019-11-17 18:45:16 +0100

    commit of the FullSky AngPow method with arbitrary window function

--- PySSC_AP.py
+++ PySSC_AP.py
@@ -8,11 +8,13 @@ import sys
 import scipy.integrate as integrate
 from scipy.interpolate import interp1d
 from classy import Class
+import json
+import os
+import subprocess as spr
 
 ##################################################
 
 # Default values for redshift bin, cosmo parameters etc
-default_zstakes = [0.9,1]
 default_cosmo_params = {'omega_b':0.022,'omega_cdm':0.12,'H0':67.,'n_s':0.96,'sigma8':0.81}
 
 # Routine to compute the Sij matrix with general window functions given as tables using AngPow for redshift integration
@@ -25,9 +27,7 @@ default_cosmo_params = {'omega_b':0.022,'omega_cdm':0.12,'H0':67.,'n_s':0.96,'si
 # Output : Sij matrix (shape: nbins x nbins)
 ## Equation used : Sij = C(ell=0,i,j)/4pi
 ## with C(ell=0,i,j) computed by AngPow
-def Sij_AngPow(z_arr, windows,Lmax, cosmo_params=default_cosmo_params,precision=10,cosmo_Class=None):
-    import subprocess as spr
-    import os
+def Sij_AngPow(z_arr,windows,Lmax,cosmo_params=default_cosmo_params,precision=12,cosmo_Class=None):
     # Assert everything as the good type and shape, and find number of redshifts, bins etc
     zz  = np.asarray(z_arr)
     win = np.asarray(windows)
@@ -51,7 +51,7 @@ def Sij_AngPow(z_arr, windows,Lmax, cosmo_params=default_cosmo_params,precision=
     else:
         cosmo = cosmo_Class
     
-    h = cosmo.h() #for  conversions Mpc/h <-> Mpc
+    h           = cosmo.h() #for  conversions Mpc/h <-> Mpc
     zofr        = cosmo.z_of_r(zz)
     comov_dist  = zofr[0] 
     keq         = 0.02/h                                          #Equality matter radiation in 1/Mpc (more or less)
@@ -65,34 +65,46 @@ def Sij_AngPow(z_arr, windows,Lmax, cosmo_params=default_cosmo_params,precision=
     kk          = np.exp(logk)                                     #logarithmic grid on k
     Pk          = np.zeros(nk)
     for ik in range(nk):
-        Pk[ik] = cosmo.pk(kk[ik],0.)                              #In Mpc^3
-    #how to obtain zmin zmax:
-    mange_min = np.zeros(win.shape[0])
-    mange_max = np.zeros(win.shape[0])
-
-    for i in range(win.shape[0]):
-        z_ald_bin = win[i,:].astype(bool)*zz
-        mange_min[i] = np.amin((z_ald_bin)[z_ald_bin!=0])
-        mange_max[i] = np.amax((z_ald_bin)[z_ald_bin!=0])
-    Sij = np.zeros((nbins,nbins))
+        Pk[ik]  = cosmo.pk(kk[ik],0.)                              #In Mpc^3
+    
+    
+    Omega_b     = default_cosmo_params["omega_b"]  /h**2
+    Omega_cdm   = default_cosmo_params["omega_cdm"]/h**2
+    Omega_m     = Omega_b + Omega_cdm
+    path        = './AngPow_files/' #loc of all AngPow stuffs
+    name        = 'SSC' #the name of the new AngPow .ini file
+    np.savetxt('%s%s_Pk.txt'%(path,name),np.transpose(np.vstack((kk/h,Pk*(h**3))))) #AngPow needs Pk(z=0)
+    cl_kmax     = np.amax(kk)*h #Cl is a mixing of modes : cl_kmax is the maximum k of the integral made by AngPow
+    
+    # define all AngPow parameters
+    ini = {'Lmin' : 0,'Lmax' : Lmax ,'linearStep' : 40, 'logStep' : 1.15,'algo_type' : 1,'limber_lthr1' : -1,'limber_lthr2' : -1,'wtype':'UserFile,UserFile','mean' : '-1.,-1.','width': '-1.,-1.','w_dir' : './AngPow_files' , 'w_files' : '%s_win1.txt , %s_win2.txt' %(name,name),'cross_depth' : -1,'n_sigma_cut' : '-1.','cl_kmax' : cl_kmax,'radial_quad'  : 'trapezes','radial_order' : 50,'chebyshev_order' : 9,'n_bessel_roots_per_interval' : 100,'h': h,'omega_matter': Omega_m ,'omega_baryon': Omega_b,'hasX' : 0,'omega_X' :'','wX':'','waX':'','cosmo_zmin' : 0.,'cosmo_zmax' : 10.,'cosmo_npts' : 1000,'cosmo_precision' : 0.001,'Lmax_for_xmin' : 2000,'jl_xmin_cut'   : 5e-10,'output_dir' : path,'common_file_tag' : 'angpow_bench_%s_'%name,'quadrature_rule_ios_dir' : '%sAngPow/data/'%(path),'power_spectrum_input_dir': path,'power_spectrum_input_file' : '%s_Pk.txt' %name,'pw_kmin' : 1e-5,'pw_kmax' : 100.,}
+    
+    # write all AngPow parameters in a .ini file
+    out = '%sangpow_bench_%s.ini'%(path,name)
+    fo  = open(out, "w")
+    for k, v in ini.items():
+         fo.write(str(k) + '='+ str(v) + '\n')
+    fo.close()
+    
+    threshold = 1e-100 # put by hands --> comming from instability in the AngPow algo
+    Sij       = np.zeros((nbins,nbins))
+    
     for bins_1 in range(nbins):
         for bins_2 in range(nbins):
-            zmin1  = mange_min[bins_1]     ; zmax1 = mange_max[bins_1]
-            zmin2  = mange_min[bins_2]     ; zmax2 = mange_max[bins_2]
-            mean1  = zmin1/2 + zmax1/2   ; mean2 = zmin2/2 + zmax2/2
-            width1 = mean1 - zmin1       ; width2 = mean2 - zmin2
-            Omega_b   = default_cosmo_params["omega_b"]  /h**2
-            Omega_cdm = default_cosmo_params["omega_cdm"]/h**2
-            Omega_m   = Omega_b + Omega_cdm
-            path      = './AngPow_files/'
-            name      = 'SSC' #the name of the new .ini file
-            np.savetxt('%s%s.txt'%(path,name),np.transpose(np.vstack((kk/h,Pk*(h**3)))))
-            cl_kmax   = np.amax(h*kk)*h
-            spr.call(['cp', '%sangpow_bench_generator.ini'%path, '%sangpow_bench_%s.ini'%(path,name)])
-            ini       = open('%sangpow_bench_%s.ini'%(path,name), "a")
-            ini.write("\nh = %s\nomega_matter =%s\nomega_baryon = %s\noutput_dir = %s\ncommon_file_tag = angpow_bench_%s_\npower_spectrum_input_dir = %s\npower_spectrum_input_file = %s.txt\nmean = %lf,%lf\nwidth = %lf,%lf\nLmax = %i\ncl_kmax = %lf"%(h,Omega_m,Omega_b,path,name,path,name,mean1,mean2,width1,width2,Lmax,cl_kmax))
-            ini.close()
+            np.savetxt('%s%s_win1.txt'%(path,name),np.transpose(np.vstack((zz[win[bins_1,:]>threshold],(win[bins_1,:])[win[bins_1,:]>threshold]))))
+            np.savetxt('%s%s_win2.txt'%(path,name),np.transpose(np.vstack((zz[win[bins_2,:]>threshold],(win[bins_2,:])[win[bins_2,:]>threshold]))))
+            # running AngPow
             os.system('%sAngPow/bin/angpow %sangpow_bench_%s.ini'%(path,path,name))
+            # Take the [0-1] column of the txt file (X-C_ell's)
             l_angpow,cl_angpow = np.loadtxt('%sangpow_bench_%s_cl.txt'%(path,name),ndmin=2,unpack=True)
-            Sij[bins_1,bins_2] = cl_angpow[0] / (4*pi)
-    return Sij
+            Sij[bins_1,bins_2] = cl_angpow[0] / (4*pi) #So far in full sky only need C_{ell=0}
+    
+    os.remove('%sangpow_bench_%s_cl.txt'%(path,name))
+    os.remove('%sangpow_bench_%s_ctheta.txt'%(path,name))
+    os.remove('%sangpow_bench_%s_used-param.txt'%(path,name))
+    os.remove('%sangpow_bench_%s.ini'%(path,name))
+    os.remove('%s%s_Pk.txt'%(path,name))
+    os.remove('%s%s_win1.txt'%(path,name))
+    os.remove('%s%s_win2.txt'%(path,name))
+    
+    return Sij;
