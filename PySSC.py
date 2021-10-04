a3f95b0062537dbc4f823e5227b7cc44dcc53b4a
Fabien Lacasa <fabien.lacasa@universite-paris-saclay.fr>
2021-10-04 19:16:44 +0200

    Copied AngPow functions from PySSC_AP.py into PySSC.py, renamed them to use same naming convention, cleaned up unused stuff. Checked that the new functions give the same output as those of PySSC_AP under the same conditions (careful that PySSC_AP does not have the same default cosmo parameters and precision settings).

--- PySSC.py
+++ PySSC.py
@@ -9,6 +9,7 @@ import math ; pi=math.pi
 import numpy as np
 import scipy.integrate as integrate
 from scipy.interpolate import interp1d
+import os
 from classy import Class
 
 ##################################################
@@ -23,7 +24,9 @@ default_cosmo_params = {'omega_b':0.022,'omega_cdm':0.12,'H0':67.,'n_s':0.96,'si
 ####################################################################################################
 
 def Sij(z_arr, windows, sky='full', clmask=None, mask=None, cosmo_params=default_cosmo_params, cosmo_Class=None, convention=0, precision=10, var_tol=0.05, verbose=False, debug=False):
+    
     test_zw(z_arr,windows)
+    
     if sky.casefold() in ['full','fullsky','full sky','full-sky']:
         Sij=Sij_fullsky(z_arr, windows, cosmo_params=cosmo_params, cosmo_Class=cosmo_Class, convention=convention, precision=precision)
     elif sky.casefold() in ['psky','partial sky','partial-sky','partial','masked']:
@@ -34,17 +37,21 @@ def Sij(z_arr, windows, sky='full', clmask=None, mask=None, cosmo_params=default
     return Sij   
 
 def Sij_alt(z_arr, windows, sky='full', cosmo_params=default_cosmo_params, cosmo_Class=None, convention=0):
+    
     test_zw(z_arr,windows)
+    
     if sky.casefold() in ['full','fullsky','full sky','full-sky']:
         Sij=Sij_alt_fullsky(z_arr, windows, cosmo_params=cosmo_params, cosmo_Class=cosmo_Class, convention=convention)
     elif sky.casefold() in ['psky','partial sky','partial-sky','partial','masked']:
-        raise Exception('No current implementation of Sij_alt for partial sky. Use Sij instead.')
+        raise Exception('No implementation of Sij_alt for partial sky. Use Sij instead.')
     else:
         raise Exception('Invalid string given for sky geometry parameter. Must be full sky (or abbreviations, see code for details).')
     return Sij
 
 def Sijkl(z_arr, windows, sky='full', clmask=None, mask=None, cosmo_params=default_cosmo_params, cosmo_Class=None, convention=0, precision=10, tol=1e-3, var_tol=0.05, verbose=False, debug=False):
+    
     test_zw(z_arr,windows)
+    
     if sky.casefold() in ['full','fullsky','full sky','full-sky']:
         Sijkl=Sijkl_fullsky(z_arr, windows, cosmo_params=cosmo_params, cosmo_Class=cosmo_Class, convention=convention, precision=precision, tol=tol)
     elif sky.casefold() in ['psky','partial sky','partial-sky','partial','masked']:
@@ -913,6 +920,246 @@ def Sijkl_psky(z_arr, windows, clmask=None, mask=None, cosmo_params=default_cosm
     
     return Sijkl
 
+####################################################################################################
+#################################          ANGPOW ROUTINES         #################################
+####################################################################################################
+
+##### Sij_AngPow_fullsky #####
+def Sij_AngPow_fullsky(z_arr,windows,cosmo_params=default_cosmo_params,precision=10,cosmo_Class=None):
+    """
+    Routine to compute the Sij matrix with general window functions given as tables
+    example : weak lensing, or galaxy clustering with redshift errors
+
+    Inputs :
+    - redshifts and window functions
+      Format : one table of redshifts with size nz, one 2D table for the collection of window functions with shape (nbins,nz)
+    - cosmology or cosmological parameters
+      Format : dictionnary with format of CLASS's wrapper classy
+    Output : Sij matrix (shape: nbins x nbins)
+
+    Options :
+    - precision : drives the number of Fourier wavenumbers in internal integrals. nk=2^precision
+
+    Equation used :  Sij = 1/(2*pi^2) int k^2 dk P(k) U(i,k)/Inorm(i) U(j,k)/Inorm(j)
+    with Inorm(i) = int dX window(i,z)^2 and U(i,k) = int dX window(i,z)^2 growth(z) j_0(kr)
+    This can also be seen as an angular power spectrum : Sij = C(ell=0,i,j)^S/4pi
+    with C(ell=0,i,j)^S = 2/pi int k^2 dk P(k) U(i,k)/Inorm(i) U(j,k)/Inorm(j)
+
+    dX depends on the convention used to define the window functions : Cl(i,j) = int dX window(i,z) window(j,z) P(k=(ell+1/2)/r,z)
+    Convention 0 (only implemented yet) : dX = dV = dV/dz dz = r^2(z) dr/dz dz. Used in Lacasa & Grain 2019.
+    """
+        
+    test_zw(z_arr, windows)
+
+    zz  = np.asarray(z_arr)
+    win = np.asarray(windows)
+    nz    = len(zz)
+    nbins = win.shape[0]    
+    
+    # If the cosmology is not provided (in the same form as CLASS), run CLASS
+    if cosmo_Class is None:
+        cosmo = Class()
+        dico_for_CLASS = cosmo_params
+        dico_for_CLASS['output'] = 'mPk'
+        cosmo.set(dico_for_CLASS)
+        cosmo.compute()
+    else:
+        cosmo = cosmo_Class
+    
+    h           = cosmo.h() #for  conversions Mpc/h <-> Mpc
+    zofr        = cosmo.z_of_r(zz)
+    comov_dist  = zofr[0]                                  #Comoving distance r(z) in Mpc
+    dcomov_dist = 1/zofr[1]                                 #Derivative dr/dz in Mpc
+    dV          = comov_dist**2 * dcomov_dist 
+    keq         = 0.02/h                                          #Equality matter radiation in 1/Mpc (more or less)
+    klogwidth   = 10                                              #Factor of width of the integration range. 10 seems ok
+    kmin        = min(keq,1./comov_dist.max())/klogwidth
+    kmax        = max(keq,1./comov_dist.min())*klogwidth
+    nk          = 2**precision                                    #10 seems to be enough. Increase to test precision, reduce to speed up.
+    #kk          = np.linspace(kmin,kmax,num=nk)                   #linear grid on k
+    logkmin     = np.log(kmin) ; logkmax   = np.log(kmax)
+    logk        = np.linspace(logkmin,logkmax,num=nk)
+    kk          = np.exp(logk)                                     #logarithmic grid on k
+    Pk          = np.zeros(nk)
+    for ik in range(nk):
+        Pk[ik]  = cosmo.pk(kk[ik],0.)                              #In Mpc^3
+    
+    
+    Omega_b     = default_cosmo_params["omega_b"]  /h**2
+    Omega_cdm   = default_cosmo_params["omega_cdm"]/h**2
+    Omega_m     = Omega_b + Omega_cdm
+    path        = './AngPow_files/' #loc of all AngPow stuffs
+    name        = 'SSC' #the name of the new AngPow .ini file
+    np.savetxt('%s%s_Pk.txt'%(path,name),np.transpose(np.vstack((kk/h,Pk*(h**3))))) #AngPow needs Pk(z=0)
+    cl_kmax     = np.amax(kk)*h #Cl is a mixing of modes : cl_kmax is the maximum k of the integral made by AngPow
+    
+    # define all AngPow parameters
+    ini = {'Lmin' : 0,'Lmax' : 1 ,'linearStep' : 40, 'logStep' : 1.15,'algo_type' : 1,'limber_lthr1' : -1,'limber_lthr2' : -1,'wtype':'UserFile,UserFile','mean' : '-1.,-1.','width': '-1.,-1.','w_dir' : './AngPow_files' , 'w_files' : '%s_win1.txt , %s_win2.txt' %(name,name),'cross_depth' : -1,'n_sigma_cut' : '-1.','cl_kmax' : cl_kmax,'radial_quad'  : 'trapezes','radial_order' : 50,'chebyshev_order' : 9,'n_bessel_roots_per_interval' : 100,'h': h,'omega_matter': Omega_m ,'omega_baryon': Omega_b,'hasX' : 0,'omega_X' :'','wX':'','waX':'','cosmo_zmin' : 0.,'cosmo_zmax' : 10.,'cosmo_npts' : 1000,'cosmo_precision' : 0.001,'Lmax_for_xmin' : 2000,'jl_xmin_cut'   : 5e-10,'output_dir' : path,'common_file_tag' : 'angpow_bench_%s_'%name,'quadrature_rule_ios_dir' : '%sAngPow/data/'%(path),'power_spectrum_input_dir': path,'power_spectrum_input_file' : '%s_Pk.txt' %name,'pw_kmin' : 1e-5,'pw_kmax' : 1,}
+    
+    # write all AngPow parameters in a .ini file
+    out = '%sangpow_bench_%s.ini'%(path,name)
+    fo  = open(out, "w")
+    for k, v in ini.items():
+         fo.write(str(k) + '='+ str(v) + '\n')
+    fo.close()
+    
+    Sij       = np.zeros((nbins,nbins))
+    
+    for bins_1 in range(nbins):
+        for bins_2 in range(nbins):
+            if bins_1 >= bins_2 :
+                z_1 = zz[win[bins_1,:]!=0]
+                w_1 = (win[bins_1,:])[win[bins_1,:]!=0]
+                z_2 = zz[win[bins_2,:]!=0]
+                w_2 = (win[bins_2,:])[win[bins_2,:]!=0]
+                np.savetxt('%s%s_win1.txt'%(path,name),np.transpose(np.vstack((z_1,w_1**2))))
+                np.savetxt('%s%s_win2.txt'%(path,name),np.transpose(np.vstack((z_2,w_2**2))))
+                # running AngPow
+                os.system('%sAngPow/bin/angpow %sangpow_bench_%s.ini'%(path,path,name))
+                # Take the [0-1] column of the txt file (X-C_ell's)
+                l_angpow,cl_angpow = np.loadtxt('%sangpow_bench_%s_cl.txt'%(path,name),ndmin=2,unpack=True)
+                Sij[bins_1,bins_2] = cl_angpow/(4*np.pi)
+                Sij[bins_2,bins_1] = Sij[bins_1,bins_2]
+            else: pass
+    
+    os.remove('%sangpow_bench_%s_cl.txt'%(path,name))
+    os.remove('%sangpow_bench_%s_ctheta.txt'%(path,name))
+    os.remove('%sangpow_bench_%s_used-param.txt'%(path,name))
+    os.remove('%sangpow_bench_%s.ini'%(path,name))
+    os.remove('%s%s_Pk.txt'%(path,name))
+    os.remove('%s%s_win1.txt'%(path,name))
+    os.remove('%s%s_win2.txt'%(path,name))
+    
+    return Sij
+
+##### Sij_AngPow_psky #####
+def Sij_AngPow_psky(z_arr,windows,clmask=None,mask=None,cosmo_params=default_cosmo_params,cosmo_Class=None,var_tol=0.05,verbose=False,debug=False): #Lmax
+    """
+    Sij_AngPow_psky
+    Routine to compute the Sij matrix in partial sky
+
+    Inputs :
+    - redshifts and window functions
+      Format : one table of redshifts with size nz, one 2D table for the collection of window functions with shape (nbins,nz)
+    - cosmology or cosmological parameters
+      Format : dictionnary with format of CLASS's wrapper classy
+    - mask or its angular power spectrum
+      Format : fits file
+    Output : Sij matrix (shape: nbins x nbins)
+    Options :
+    - var_tol : precision setting to decide where to cut the sum over multipoles. Final precision on Sij should be better than var_tol. Default is 5%.
+
+    Equation used : Sij = sum_ell (2ell+1) C(ell,i,j) C(ell,mask) /(4pi*fsky)^2
+    where C(ell,i,j) = 2/pi \int kk^2 dkk P(kk) U(i;kk,ell)/Inorm(i) U(j;kk,ell)/Inorm(j) is computed by AngPow
+    with Inorm(i) = int dX window(i,z)^2  and U(i;kk,ell) = int dX window(i,z)^2 growth(z) j_ell(kk*r)
+    """
+
+    import healpy as hp
+    from scipy.special import spherical_jn as jn
+    
+    test_zw(z_arr, windows)
+    
+    zz  = np.asarray(z_arr)
+    win = np.asarray(windows)    
+    nz    = len(zz)
+    nbins = win.shape[0]
+    
+    test_mask(mask, clmask)
+
+    if mask is None: # User gives Cl(mask)
+        if verbose:
+            print('Using Cls given as a fits file')
+        cl_mask = hp.fitsfunc.read_cl(str(clmask))
+        ell = np.arange(len(cl_mask))
+        lmaxofcl = ell.max()
+    else : # User gives mask as a map
+        if verbose:
+            print('Using mask map, given as a fits file')
+        map_mask = hp.read_map(str(mask),verbose=False)
+        nside    = hp.pixelfunc.get_nside(map_mask)
+        lmaxofcl = 2*nside
+        cl_mask  = hp.anafast(map_mask, lmax=lmaxofcl)
+        ell      = np.arange(lmaxofcl+1)
+    
+    # compute fsky from the mask
+    fsky = np.sqrt(cl_mask[0]/(4*pi))
+    if verbose:
+        print('f_sky = %.4f' %(fsky))
+
+    # Search of the best lmax for all later sums on ell
+    lmax = find_lmax(ell,cl_mask,var_tol,debug=debug)
+    if lmax == 0 : lmax = 1
+    
+    if verbose:
+        print('lmax = %i' %(lmax))
+
+    # Cut ell and Cl_mask to lmax, for all later computations
+    cl_mask = cl_mask[:(lmax+1)]
+    nell    = lmax+1
+    ell     = np.arange(nell) #0..lmax
+        
+    # If the cosmology is not provided (in the same form as CLASS), run CLASS
+    if cosmo_Class is None:
+        cosmo = Class()
+        dico_for_CLASS = cosmo_params
+        dico_for_CLASS['output'] = 'mPk'
+        cosmo.set(dico_for_CLASS)
+        cosmo.compute()
+    else:
+        cosmo = cosmo_Class
+    
+    h           = cosmo.h() #for  conversions Mpc/h <-> Mpc
+    zofr        = cosmo.z_of_r(zz)
+    comov_dist  = zofr[0] 
+    kk = np.geomspace(1e-4,1,1000)
+    Pk = np.zeros(len(kk))
+    for ik in range(len(kk)): Pk[ik]  = cosmo.pk(kk[ik]*h,0.)
+    path        = './AngPow_files/' #loc of all AngPow stuffs
+    name        = 'SSC' #the name of the new AngPow .ini file
+    np.savetxt('%s%s_Pk.txt'%(path,name),np.transpose(np.vstack((kk,Pk*h**3)))) #AngPow needs Pk(z=0)
+    
+    # define all AngPow parameters
+    ini = {'Lmin' : 0,'Lmax' : lmax ,'linearStep' : 40, 'logStep' : 1.15,'algo_type' : 1,'limber_lthr1' : -1,'limber_lthr2' : -1,'wtype':'UserFile,UserFile','mean' : '-1.,-1.','width': '-1.,-1.','w_dir' : './AngPow_files' , 'w_files' : '%s_win1.txt , %s_win2.txt' %(name,name),'cross_depth' : -1,'n_sigma_cut' : '-1.','cl_kmax' : np.amax(kk)*h,'radial_quad'  : 'gausskronrod','radial_order' : 400,'chebyshev_order' : 9,'n_bessel_roots_per_interval' : 100,'h': h,'omega_matter': cosmo.Omega_m() ,'omega_baryon': cosmo.Omega_b(),'hasX' : 0,'omega_X' :'','wX':'','waX':'','cosmo_zmin' : 0.,'cosmo_zmax' : 10.,'cosmo_npts' : 1000,'cosmo_precision' : 0.001,'Lmax_for_xmin' : 2000,'jl_xmin_cut'   : 5e-10,'output_dir' : path,'common_file_tag' : 'angpow_bench_%s_'%name,'quadrature_rule_ios_dir' : '%sAngPow/data/'%(path),'power_spectrum_input_dir': path,'power_spectrum_input_file' : '%s_Pk.txt' %name,'pw_kmin' : kk[0],'pw_kmax' : kk[-1]}
+    
+    # write all AngPow parameters in a .ini file
+    out = '%sangpow_bench_%s.ini'%(path,name)
+    fo  = open(out, "w")
+    for k, v in ini.items():
+         fo.write(str(k) + '='+ str(v) + '\n')
+    fo.close()
+    
+    Sij       = np.zeros((nbins,nbins))
+    
+    for bins_1 in range(nbins):
+        for bins_2 in range(nbins):
+            if bins_1 >= bins_2 :
+                z_1 = zz[win[bins_1,:]!=0]
+                w_1 = (win[bins_1,:])[win[bins_1,:]!=0]
+                z_2 = zz[win[bins_2,:]!=0]
+                w_2 = (win[bins_2,:])[win[bins_2,:]!=0]
+                comov_dist_ = comov_dist[win[bins_1,:]!=0]
+                np.savetxt('%s%s_win1.txt'%(path,name),np.transpose(np.vstack((z_1,w_1))))
+                np.savetxt('%s%s_win2.txt'%(path,name),np.transpose(np.vstack((z_2,w_2))))
+                #np.savetxt('%s%s_win1.txt'%(path,name),np.transpose(np.vstack((zz,((win[bins_1,:]))**2))))
+                #np.savetxt('%s%s_win2.txt'%(path,name),np.transpose(np.vstack((zz,((win[bins_2,:]))**2))))
+                # running AngPow
+                os.system('%sAngPow/bin/angpow %sangpow_bench_%s.ini'%(path,path,name))
+                # Take the [0-1] column of the txt file (X-C_ell's)
+                l_angpow,cl_angpow = np.loadtxt('%sangpow_bench_%s_cl.txt'%(path,name),ndmin=2,unpack=True)
+                #Sij[bins_1,bins_2] = cl_angpow[0] / (4*pi) #So far in full sky only need C_{ell=0}
+                Sij[bins_1,bins_2] = (1/(4*np.pi*fsky))**2*np.sum((2*l_angpow+1)*cl_mask[0:lmax]*cl_angpow)
+                Sij[bins_2,bins_1] = Sij[bins_1,bins_2]
+            else: pass
+    
+    os.remove('%sangpow_bench_%s_cl.txt'%(path,name))
+    os.remove('%sangpow_bench_%s_ctheta.txt'%(path,name))
+    os.remove('%sangpow_bench_%s_used-param.txt'%(path,name))
+    os.remove('%sangpow_bench_%s.ini'%(path,name))
+    os.remove('%s%s_Pk.txt'%(path,name))
+    os.remove('%s%s_win1.txt'%(path,name))
+    os.remove('%s%s_win2.txt'%(path,name))
+    
+    return Sij
 
 ####################################################################################################
 #################################        AUXILIARY ROUTINES        #################################
